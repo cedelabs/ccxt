42809df789f5e0d8514e03de65b2601904dcb22b
eyusd <61081062+eyusd@users.noreply.github.com>
2023-07-06 12:41:30 +0200

    feat(store): store-1119 - bitget proxy & tests (#15)

--- ts/src/bitget.ts
+++ ts/src/bitget.ts
@@ -1056,6 +1056,14 @@ export default class bitget extends Exchange {
         });
     }
 
+    // Override
+    isUsingForcedProxy (params = {}, api = 'public') {
+        if (api[0] === 'private') {
+            return true;
+        }
+        return false;
+    }
+
     setSandboxMode (enabled) {
         this.options['sandboxMode'] = enabled;
     }
