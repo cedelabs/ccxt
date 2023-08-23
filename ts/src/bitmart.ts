18c01c3df27539c831421381af4b79d106824e86
Mathieu Raimbault <60718973+Mario2206@users.noreply.github.com>
2023-08-23 15:45:02 +0200

    fix: solana network for bitmart (#23)

--- ts/src/bitmart.ts
+++ ts/src/bitmart.ts
@@ -415,7 +415,7 @@ export default class bitmart extends Exchange {
                     'FIO': 'FIO',
                     'SCRT': 'SCRT',
                     'IOTX': 'IOTX',
-                    'SOL': 'SOL',
+                    'SOL': 'SPL',
                     'ALGO': 'ALGO',
                     'ATOM': 'ATOM',
                     'DOT': 'DOT',
