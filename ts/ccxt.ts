6ae08a80a228ddbc86a46b6c0353bd20eeb5aa40
Clément Juventin <clementjuventin@gmail.com>
2024-03-14 16:18:56 +0100

    throttler export

--- ts/ccxt.ts
+++ ts/ccxt.ts
@@ -36,6 +36,7 @@ import * as errors   from './src/base/errors.js'
 import type { Market, Trade , Fee, Ticker, OrderBook, Order, Transaction, Tickers, Currency, Balance, DepositAddress, WithdrawalResponse, DepositAddressResponse, OHLCV, Balances, PartialBalances, Dictionary, MinMax, Position, FundingRateHistory, Liquidation, FundingHistory, MarginMode, Greeks, CurrencyInterface, MarketInterface } from './src/base/types.js'
 import { BaseError, ExchangeError, PermissionDenied, AccountNotEnabled, AccountSuspended, ArgumentsRequired, BadRequest, BadSymbol, MarginModeAlreadySet, BadResponse, NullResponse, InsufficientFunds, InvalidAddress, InvalidOrder, OrderNotFound, OrderNotCached, CancelPending, OrderImmediatelyFillable, OrderNotFillable, DuplicateOrderId, NotSupported, NetworkError, DDoSProtection, RateLimitExceeded, ExchangeNotAvailable, OnMaintenance, InvalidNonce, RequestTimeout, AuthenticationError, AddressPending, NoChange }  from './src/base/errors.js'
 import { CustomThrottlePriority } from './src/base/types.js'
+import { Throttler } from './src/base/functions/customThrottle.js'
 
 
 //-----------------------------------------------------------------------------
@@ -457,6 +458,7 @@ export {
     MinMax,
     Position,
     CustomThrottlePriority,
+    Throttler,
     FundingRateHistory,
     Liquidation,
     FundingHistory,
